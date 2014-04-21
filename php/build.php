8fa4bc783f553278e5766f0a5421b3598e2a7dcb
Simon Asika <asika32764@gmail.com>
2014-04-22 00:28:50 +0800

    Add 3 fonts

---
+++ php/build.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Part of chinese-font-size project. 
+ *
+ * @copyright  Copyright (C) 2011 - 2014 SMS Taiwan, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+$font   = 'Microsoft YaHei';
+$weight = 'normal';
+$file   = 'YaHei-' . $weight . '.html';
+$start  = 10;
+$end    = 100;
+
+$tmpl = <<<TMPL
+<p style="font-size: %spx; font-weight: %s;">
+	%spx: 微風迎客，軟語伴茶。
+</p>
+
+TMPL;
+
+$layout = <<<LAYOUT
+<div style="font-family: '%s';">
+	<h1>%s</h1>
+
+%s
+</div>
+LAYOUT;
+
+$html = array();
+
+foreach (range($start, $end) as $size)
+{
+	$html[] = sprintf($tmpl, $size, $weight, $size);
+}
+
+$output = sprintf($layout, $font, $font, implode('', $html));
+
+file_put_contents(__DIR__ . '/../' . $file, $output);
+
+echo sprintf("Building font: %s success.\n\n", $font);
